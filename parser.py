7b5d35942fa085bb9b7900413af639275be389b0
Artem Bogdan <temych009@gmail.com>
2020-06-07 00:39:09 +0300

    Recurcievly search for new links, has problems with non ascii characters

--- parser.py
+++ parser.py
@@ -8,6 +8,7 @@ import os
 def open_link(path):
     """Open the link and return the bs object for that link or None"""
     try:
+        print(f"Trying to open {path}....")
         html = urlopen(path)
         bs = BeautifulSoup(html.read(), "html.parser")
     except HTTPError as e:
@@ -15,7 +16,7 @@ def open_link(path):
     except URLError:
         print("The server could not be found")
     else:
-        print(f"{path} is opened")
+        print(f"{path} is opened!")
         return bs
 
 
@@ -46,25 +47,36 @@ def get_links(bs):
 if __name__ == '__main__':
     # Collect all links
     seen = set()
-    base_page = "https://www.radiosvoboda.org"
-
-    page = open_link(base_page)
-    links = get_links(page)
+    external_links = set()
+    internal_links = set()
+    articles = set()
 
     re_external = re.compile('^(http|https)')
-    external_links = list(filter(re_external.match, links))
-
     re_internal = re.compile('^(https^|http^|#^|\/).*')
-    internal_links = list(filter(re_internal.match, links))
+    re_articles = re.compile('^\/a\/.*')
 
-    print(internal_links)
-    # Define links to explore
-## Find articles
-## Find trash links
-## Find liks to explore
+    def find_all_links(base_page):
+        global seen
+        global external_links
+        global internal_links
+        global articles
+
+        page = open_link(base_page)
+        seen.add(base_page)
+        links = get_links(page)
+
+        external_links = (external_links | set(filter(re_external.match, links)))
+        internal_links = (internal_links | set(filter(re_internal.match, links)))
+        articles = (articles | set(filter(re_articles.match, links)))
+
+        if internal_links - seen == set():
+            print("Reached the base case, returning...")
+        else:
+            for link in internal_links:
+                full_path = urljoin(base_page, link)
+                if full_path not in seen:
+                    find_all_links(full_path)
 
 
-# Open defined links
-
-# Check if you have new links
-
+    find_all_links("https://www.radiosvoboda.org")
+    print(articles)
